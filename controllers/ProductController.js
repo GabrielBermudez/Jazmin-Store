dbe674369a593b8e8e399d9823f30a4e2293f9c9
Gabriel M. Bermudez <gbermudez@kafesistemas.com.ar>
2021-03-29 22:44:28 -0300

    Upload changes about connection with mongose and mongoDB atlas, and lot changes

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,5 +1,7 @@
-var Product = require('../models/ProductModel');
+let Product = require('../models/ProductModel');
 let mongoose = require('mongoose');
+let mv = require('mv');
+const fs = require('fs').promises;
 
 exports.action_product_index = function (req, res) {
     Product.FindProductAll(function(err,products) {
@@ -23,13 +25,7 @@ exports.action_product_view = function (req, res) {
 
 exports.action_product_create = function (req, res) {
 
-    Product.FindProductByID(req.params.id, function(err,product) {
-        if (err) {
-            return next(err);
-        }
-
-     	res.render('admin/product/create',{ product });
-    });
+    res.render('admin/product/create',{ product:null });
 };
 
 exports.action_product_update = function (req, res) {
@@ -44,17 +40,22 @@ exports.action_product_update = function (req, res) {
 };
 
 exports.product_create = function(req, res) {
+    let date = new Date();
+
+    UploadFile(req);
+
 	let product = new Product({
 		nombre: req.body.nombre,
 		descripcion: req.body.descripcion,
 		precio: req.body.precio,
 		stock: req.body.stock,
 		categoria: req.body.categoria,
-		fecha_creacion: req.body.fecha_creacion,
-		fecha_actualizacion: req.body.fecha_actualizacion,
-		estado: req.body.estado,
+		fecha_creacion: date,
+		fecha_actualizacion: date,
+		disponibilidad: req.body.disponibilidad,
+        url: `/images/${req.body.categoria}/${req.files.image.name}`,
 	});
-    console.log(product);
+    console.log(product,date);
 	Product.AddProduct(product,function(err){
 		if(err){
 			return next(err);
@@ -65,35 +66,93 @@ exports.product_create = function(req, res) {
 };
 
 exports.product_update = function(req, res) {
+    let date = new Date();
+
 	Product.FindProductByID(req.params.id, function(err,product) {
         if (err) {
             return next(err);
         }
+        
+        if(req.files === null){
 
-     	product.nombre = req.body.nombre;
-		product.descripcion = req.body.descripcion;
-		product.precio = req.body.precio;
-		product.stock = req.body.stock;
-		product.categoria = req.body.categoria;
-		product.fecha_creacion = req.body.fecha_creacion;
-		product.fecha_actualizacion = req.body.fecha_actualizacion;
-		product.estado = req.body.estado;
+         	product.nombre = req.body.nombre;
+            product.descripcion = req.body.descripcion;
+            product.precio = req.body.precio;
+            product.stock = req.body.stock;
+            product.categoria = req.body.categoria;
+            product.fecha_actualizacion = date;
+            product.disponibilidad = req.body.disponibilidad;
 
-     	product.save();
+         	product.save();
 
-     	console.log('Product Updated Successfully');
-		res.redirect('/admin/product/index');
+         	console.log('Product Updated Successfully');
+    		res.redirect('/admin/product/index');
+
+        }else{
+            UploadFile(req);
+            fs.unlink(`./public${product.url}`)
+                .then(() => {
+                    console.log(`Image of ${product.nombre} has removed`)
+                    product.nombre = req.body.nombre;
+                    product.descripcion = req.body.descripcion;
+                    product.precio = req.body.precio;
+                    product.stock = req.body.stock;
+                    product.categoria = req.body.categoria;
+                    product.fecha_actualizacion = date;
+                    product.disponibilidad = req.body.disponibilidad;
+                    product.url = `/images/${req.body.categoria}/${req.files.image.name}`;
+                    product.save();
+                    res.redirect('/admin/product/index');
+
+                }).catch(err => {
+                    console.error('Something wrong happened removing the file', err)
+                    res.redirect('/admin/product/index');
+            })
+        }
 
     });
 };
 
 exports.product_delete = function (req, res) {
-	Product.RemoveProductByID(req.params.id, function(err){
-		if (err) {
+    Product.FindProductByID(req.params.id, function(err,product) {
+        if (err) {
+            return next(err);
+        }
+
+        fs.unlink(`./public${product.url}`)
+          .then(() => {
+            console.log(`Image of ${product.nombre} has removed`)
+            RemoveProduct(product.id);
+
+            res.redirect('/admin/product/index');
+
+          }).catch(err => {
+            console.error('Something wrong happened removing the file', err)
+            res.redirect('/admin/product/index');
+          })
+    });
+
+};
+
+
+function RemoveProduct(id){
+    console.log(id);
+    Product.RemoveProductByID(id, function(err){
+        if (err) {
             return next(err);
         }
         console.log('Product Deleted Successfully');
-        res.redirect('/admin/product/index');
-	});
-};
+    });
+}
 
+function UploadFile(req,){
+    let oldPath = req.files.image.tempFilePath;
+    let newPath = `./public/images/${req.body.categoria}/${req.files.image.name}`;
+
+    mv(oldPath, newPath, {mkdirp: true}, function(err) {
+        if(err) 
+            return res.status(500).send({ message : err })
+
+        console.log(`File ${req.files.image.name} has moved in public/images/...`)
+    });
+}
